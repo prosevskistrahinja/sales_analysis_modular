2bd0713d1b0d655e326494ad29cc249da7aa13da
Strahinja Prosevski <prosevskistrahinja@gmail.com>
2026-03-18 22:50:57 +0100

    Added analysis function on branch

--- sales_data.py
+++ sales_data.py
@@ -1,3 +1,9 @@
 #sales_data.py
 sales_data = [230,200,310,290,400,150,180]
 print(f"Sales data: {sales_data}")
+def total_sales(data):
+    return sum(data)
+def average_sales(data):
+    return sum(data) / len(data)
+print(f"Total sales: {total_sales(sales_data)}")
+print(f"Average sales: {average_sales(sales_data)}")
